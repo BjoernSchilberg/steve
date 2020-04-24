7a795a571cc1c461c59b0c4f2039975614ec2a6e
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2020-04-24 11:20:17 +0200

    Some cleanup and added some real-world maintanance.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"strings"
 	"time"
 
@@ -12,6 +13,29 @@ import (
 
 	_ "layeh.com/gumble/opus"
 )
+
+var daysToGerman = map[time.Weekday]string{
+	time.Monday:    "Montag",
+	time.Tuesday:   "Dienstag",
+	time.Wednesday: "Mittwoch",
+	time.Thursday:  "Donnerstag",
+	time.Friday:    "Freitag",
+	time.Saturday:  "Samstag",
+	time.Sunday:    "Sonntag",
+}
+
+func daytimeGreet(t time.Time) string {
+	switch {
+	case t.Hour() > 5 && t.Hour() < 12:
+		return "Guten Morgen!"
+	case t.Hour() >= 12 && t.Hour() <= 13:
+		return "Mahlzeit!"
+	case t.Hour() > 13 && t.Hour() < 17:
+		return "Hallo!"
+	default:
+		return "Guten Abend!"
+	}
+}
 
 func main() {
 	var stream *gumbleffmpeg.Stream
@@ -27,40 +51,18 @@ func main() {
 		},
 
 		TextMessage: func(e *gumble.TextMessageEvent) {
-			t := time.Now()
-			var tageszeit string
-			switch {
-			case t.Hour() > 5 && t.Hour() < 12:
-				tageszeit = "Guten Morgen!"
-			case t.Hour() >= 12 && t.Hour() <= 13:
-				tageszeit = "Mahlzeit!"
-			case t.Hour() > 13 && t.Hour() < 17:
-				tageszeit = "Hallo!"
-			default:
-				tageszeit = "Guten Abend!"
-			}
-			r := strings.NewReplacer(
-				"Monday", "Montag",
-				"Tuesday", "Dienstag",
-				"Wednesday", "Mittwoch",
-				"Thursday", "Donnerstag",
-				"Friday", "Freitag",
-				"Saturday", "Samstag",
-				"Sunday", "Sonntag")
+			now := time.Now()
 
 			if e.Sender.Channel.Name != "Küche" {
 				e.Sender.Send(e.Sender.Name + "! Komme doch bitte in die Küche!")
 				return
 			}
 
-			if e.Sender == nil {
+			if e.Sender == nil || !strings.Contains(strings.ToLower(e.Message), "kaffee") {
 				return
 			}
-			lowerS, lowerWord := strings.ToLower(e.Message), strings.ToLower("kaffee")
-			ok := strings.Contains(lowerS, lowerWord)
-			if !ok {
-				return
-			}
+
+			tageszeit := daytimeGreet(now)
 			e.Sender.Send(tageszeit + " " + e.Sender.Name + "! Dein Kaffee wird nun kredenzt ... ")
 			if stream != nil && stream.State() == gumbleffmpeg.StatePlaying {
 				return
@@ -70,9 +72,23 @@ func main() {
 				fmt.Printf("%s\n", err)
 			}
 			stream.Wait()
-			weekday := time.Now().Weekday()
-			wochentag := r.Replace(weekday.String())
+
+			wochentag := daysToGerman[now.Weekday()]
 			e.Sender.Send("Moege Dein Kaffee heute stark und Dein " + wochentag + " kurz sein!")
+
+			r := rand.Float64()
+			if r > 2.0/3 {
+				e.Sender.Send("KAFFEESATZ LEEREN")
+			}
+			if r > 5.0/6 {
+				e.Sender.Send("WASSERTANK FUELLEN")
+			}
+			if r > 6.0/7 {
+				e.Sender.Send("BOHNEN FUELLEN")
+			}
+			if r > 7.0/8 {
+				e.Sender.Send("REINIGUNG STARTEN")
+			}
 		},
 	})
 }
